2a668e8c686181a939b296d7fa3b42e026ec7035
Thomaarz <tgirbe@iut.univ-paris8.fr>
2022-03-20 20:38:16 +0100

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = FXMLLoader.load(getClass().getResource("vue.fxml"));
+			Scene scene = new Scene(root,600,300); 
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
